b477a3948bd1318a93a2d57ca13c95e5703ca1f1
Michelle1712 <87138771+Michelle1712@users.noreply.github.com>
2021-07-20 08:54:19 -0500

    Initial Commit

---
+++ Patient Medical App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patient Medical App
+//
+//  Created by Student26 on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
